c4bddc1eed48c4365b19d413f1d88746398aad4f
Smiley <Smiley@dhcp-10-38-240-90.daap.uc.edu>
2013-07-12 09:14:51 -0400

    Worked on Users and security constraints. Added a test jsp page to consolidate adding and removing library items.

---
+++ src/java/WebTier/LoginManagerBean.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package WebTier;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Smiley
+ */
+@ManagedBean
+@SessionScoped
+public class LoginManagerBean {
+
+    /**
+     * Creates a new instance of LoginManagerBean
+     */
+    public LoginManagerBean() {
+    }
+}
